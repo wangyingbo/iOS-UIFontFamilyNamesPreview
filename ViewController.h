a293b6849e959053f6ba581870e0bfee8ecfa83b
sushushu <1404565175@qq.com>
2016-07-24 04:17:56 +0800

    第一版,下版加入点击预览各个字体种中文,英文,数字,特殊字符下的显示详情.

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  font
+//
+//  Created by softlipa软嘴唇 on 16/7/18.
+//  Copyright © 2016年 softlipa软嘴唇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS系统字体大全
+//
+//  Created by softlipa软嘴唇 on 16/7/20.
+//  Copyright © 2016年 softlipa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
